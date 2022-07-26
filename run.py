4932e9fad8383c155c0b26ab9d71e2bb65b2ece8
Bella-aa <Nyashamarara28@gmail.com>
2022-07-26 23:23:46 +0000

    added questions for quiz

--- run.py
+++ run.py
@@ -15,3 +15,10 @@ def play_again():
     pass
 #------------------
 
+questions = {
+    "Which waterfall is called the smoke that thunders?:":"A",
+    "Which is the biggest waterfall in the world?:":"C",
+    "Which mountain has the tallest peak on Earth and measures 8 848.86m tall?:":"B",
+    "Where is the Grand Canyon located?:":"D"
+    "The Great Barrier Reef is the largest Coral reef located in which country?:":"B"
+}
